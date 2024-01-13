daf1512dd1deb393bc00893736f3fedacb2ee4fd
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2024-01-13 15:50:01 -0300

    feat: add config to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { app } from ".";
 
 app.listen(3333, () => {
-  console.log(`
+    console.log(`
       App is running at port 3333
-      API documentation: http://localhost:3333/api-documentation
-  `)
-})
+      API documentation: https://kenzie-empresas-api-4b38.onrender.com/api-documentation
+  `);
+});
